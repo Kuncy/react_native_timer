d85c10c9dfbd67ef61693b0d4143f85756ea1551
Kuncy <43200807+Kuncy@users.noreply.github.com>
2021-10-13 21:15:18 +0200

    Added functionallity for add/start and delete timers

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, ScrollView, Text, View } from 'react-native';
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Text,
+  KeyboardAvoidingView,
+} from 'react-native';
+
+import { newTimer } from './utils/TimerUtils';
 import EditableTimer from './components/EditableTimer';
 import ToggleableTimerForm from './components/ToggleableTimerForm';
 
 export default class App extends React.Component {
-
   state = {
     timers: [
       {
@@ -23,32 +29,119 @@ export default class App extends React.Component {
         id: uuidv4(),
         elapsed: 1273998,
         isRunning: false,
-      }
-    ]
+      },
+    ],
+  };
+
+  componentDidMount() {
+    const TIME_INTERVAL = 1000;
+
+    this.intervalId = setInterval(() => {
+      const { timers } = this.state;
+
+      this.setState({
+        timers: timers.map(timer => {
+          const { elapsed, isRunning } = timer;
+
+          return {
+            ...timer,
+            elapsed: isRunning ? elapsed + TIME_INTERVAL : elapsed,
+          };
+        }),
+      });
+    }, TIME_INTERVAL);
   }
- 
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  handleCreateFormSubmit = timer => {
+    const { timers } = this.state;
+
+    this.setState({
+      timers: [newTimer(timer), ...timers],
+    });
+  };
+
+  handleFormSubmit = attrs => {
+    const { timers } = this.state;
+
+    this.setState({
+      timers: timers.map(timer => {
+        if (timer.id === attrs.id) {
+          const { title, project } = attrs;
+
+          return {
+            ...timer,
+            title,
+            project,
+          };
+        }
+
+        return timer;
+      }),
+    });
+  };
+
+  handleRemovePress = timerId => {
+    this.setState({
+      timers: this.state.timers.filter(t => t.id !== timerId),
+    });
+  };
+
+  toggleTimer = timerId => {
+    this.setState(prevState => {
+      const { timers } = prevState;
+
+      return {
+        timers: timers.map(timer => {
+          const { id, isRunning } = timer;
+
+          if (id === timerId) {
+            return {
+              ...timer,
+              isRunning: !isRunning,
+            };
+          }
+
+          return timer;
+        }),
+      };
+    });
+  };
+
   render() {
-    const {timers} = this.state;
+    const { timers } = this.state;
 
     return (
-        <View style={styles.appContainer}>
-          <View style={styles.titleContainer}>
-            <Text style={styles.title}>Timers</Text>
-          </View>
-          <ScrollView style={styles.timerList}>
-            <ToggleableTimerForm />
-            {timers.map(({title, project, id, elapsed, isRunning}) => (
-              <EditableTimer 
+      <View style={styles.appContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Timers</Text>
+        </View>
+        <KeyboardAvoidingView
+          behavior="padding"
+          style={styles.timerListContainer}
+        >
+          <ScrollView contentContainerStyle={styles.timerList}>
+            <ToggleableTimerForm onFormSubmit={this.handleCreateFormSubmit} />
+            {timers.map(({ title, project, id, elapsed, isRunning }) => (
+              <EditableTimer
                 key={id}
                 id={id}
                 title={title}
                 project={project}
                 elapsed={elapsed}
                 isRunning={isRunning}
+                onFormSubmit={this.handleFormSubmit}
+                onRemovePress={this.handleRemovePress}
+                onStartPress={this.toggleTimer}
+                onStopPress={this.toggleTimer}
               />
             ))}
           </ScrollView>
-        </View>
+        </KeyboardAvoidingView>
+      </View>
     );
   }
 }
@@ -60,15 +153,18 @@ const styles = StyleSheet.create({
   titleContainer: {
     paddingTop: 35,
     paddingBottom: 15,
-    paddingBottomWidth: 1,
+    borderBottomWidth: 1,
     borderBottomColor: '#D6D7DA',
   },
   title: {
     fontSize: 18,
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
+  },
+  timerListContainer: {
+    flex: 1,
   },
   timerList: {
     paddingBottom: 15,
-  }
+  },
 });
